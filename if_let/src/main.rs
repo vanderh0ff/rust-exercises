c60b3e6230e668b65571bfbebd2e8ca628b3d0b4
Matthew Vander Hoff <m.vanderhoff94@gmail.com>
2022-02-20 11:10:42 -0500

    if let syntax exercise

---
+++ if_let/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    println!("Hello, world!");
+    let config_max = Some(3u8);
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {}", max);
+    }
+    /*
+     * equivilant to this but wiht less lines
+     *
+     * let config_max = Some(3u8);
+     * match config_max {
+     *     Some(max) => println!("The maximum is configured to be {}", max),
+     *     _ => (),
+     * }
+     */
+}
