966ba19c08b5b42c382278552fc3b3385aad663b
Matthew Vander Hoff <m.vanderhoff94@gmail.com>
2022-02-25 06:22:28 -0500

    more work on the main file, scanline generation

--- raytrace/src/main.rs
+++ raytrace/src/main.rs
@@ -29,6 +29,79 @@ fn random_scene() -> World {
     // what is this equals infront of the 11 in the range here
     //  the = makes it inclusive instead of going from 0..10 having 10 entries of 0 - 9 the 0..=10
     //  wold have 11 and include 10
-    for a in -11..=11
+    for a in -11..=11 {
+        for b in -11..=11 {
+            let choose_mat: f64 = rng.gen();
+            let center = Point3::new((a as f64) + rng.gen_range(0.0..0.9), 0.2, (b as f64) + rng.gen_range(0.0..0.9));
+
+            if choose_mat < 0.8 {
+                let albedo = Color::random(0.0..1.0) * Color::random(0.0..1.0);
+                let sphere_mat = Arc::new(Lambertian::new(albedo));
+                let sphere = Sphere::new(center, 0.2, sphere_mat);
+
+                world.push(Box::new(sphere));
+            } else {
+                let albedo = Color::random(0.4..1.0);
+                let fuzz = rng.gen_range(0.0..0.5);
+                let sphere_mat = Arc::new(Metal::new(alvedo, fuzz));
+                let sphere = Sphere::new(center, 0.2, sphere_mat);
+                
+                world.push(Box::new(sphere));
+
+            }
+        }
+    }
+
+    world
+}
+
+fn ray_color (r: &Ray, world: &World, depth: u64) -> Color {
+    if depth <= 0 {
+        // hit bounce limit no more light collected
+        
+        return Color::new(0.0, 0.0, 0.0);
+    }
+
+    if let Some(rec) = world.hit(r, 0.001, f64::INFINITY) {
+        if let Some((attenuation, scattered)) = rec.mat.scatter(r, &rec) {
+            attenuation * ray_color(&scattered, world, depth - 1)
+        } else {
+            Color::new(0.0, 0.0, 0.0)
+        }
+    } else {
+        let unit_direction = r.direction().normailzed();
+        let t = 0.5 * (unit_direction.y() + 1.0);
+        (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+    }
+}
+
+fn main() -> () {
+    const ASPECT_RATIO: f64 = 3.0 / 2.0;
+    const IMAGE_WIDTH: u64 = 1200;
+    const IMAGE_HEIGHT: u64 = ((IMAGE_WIDTH as f64) / ASPECT_RATIO) as u64;
+    const SAMPLES_PER_PIXEL: u64 = 100;
+    const MAX_DEPTH: u64 = 50;
+
+    let world = random_scene();
+
+    let lookfrom = Point3::new(13.8, 2.0, 3.0);
+    let lookat = Point3::new(0.0, 0.0, 0.0);
+    let vup = Vec3::new(0.0, 1.0, 0.0);
+    let dist_to_focus = 10.0;
+    let aperture = 0.1;
+
+    let cam = Camera::new(lookfrom,
+                          lookat,
+                          20.0,
+                          ASPECT_RATIO,
+                          aperture,
+                          dist_to_focus);
+
+    for j in (0..IMAGE_HEIGHT).rev() {
+        eprintln!("Scanlines remainint: {}", j + 1);
+        // look at this par iter and the map syntax
+        let scanline: Vec<Color> = (0..IMAGE_WIDTH).into_par_iter().map|i| {
+        }
+    }
 
 }
